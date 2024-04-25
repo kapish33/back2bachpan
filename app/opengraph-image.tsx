7418164a090fabbc56f316f087c1113feacef17c
Kapish Singh <59023093+kapish33@users.noreply.github.com>
2024-04-25 15:53:59 +0530

    open graph

---
+++ app/opengraph-image.tsx
@@ -0,0 +1,41 @@
+import { ImageResponse } from "next/og";
+import { NextRequest } from "next/server";
+
+export const alt = "About Acme";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+export const contentType = "image/png";
+
+export default function Image({
+  params: { category },
+}: {
+  params: { category: string };
+}): ImageResponse {
+  const post = {
+    title: category,
+  };
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 48,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <img src="../app/opengraph-image.jpeg" />
+        {/* {post.title} */}
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
